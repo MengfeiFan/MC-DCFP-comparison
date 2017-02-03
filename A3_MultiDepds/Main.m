50ef739a1ab36489dbefa27676cda46b1c95056d
MengfeiFan <mengfei.fan@hotmail.com>
2017-02-03 20:56:45 +0100

    调整NHPP代码
    NHPP代码中，较大的T的选取影响最终结果与图4的一致程度，试验中将T增至平均到达时间的4-5倍及以上时，一致性较好。

--- A3_MultiDepds/Main.m
+++ A3_MultiDepds/Main.m
@@ -8,7 +8,7 @@ mu_beta = 1e-4; sigma_beta = 1e-5;
 D = 5;
 mu_Y_0 = 1.2;
 sigma_Y = 0.2;
-alpha = .01;
+alpha = 1;
 H = 7.5;
 C = 1.5;
 delta_C = .1;
@@ -21,18 +21,18 @@ gamma_set = [0 0 0;1e-5 0 0;1e-4 0 0;1e-3 0 0];
 % gamma_set = [0 0 0;0 0 1e-5;0 0 1e-4;0 0 1e-3];
 
 for k=1:4
-    i = 0; x = 0; sum_W = 0; tau = 0; delta_tau = 0;
     gamma_b = gamma_set(k,1);
     gamma_c = gamma_set(k,2);
     gamma_d = gamma_set(k,3);
-    
+    i = 0; 
     while i < samplesize
         disp([num2str(i) '/' num2str(samplesize) 'th interations'])
+        x = 0; sum_W = 0; tau = 0; delta_tau = 0;mu_Y = mu_Y_0;
     %% Step 2
         beta_s = normrnd(mu_beta,sigma_beta);
         while 1
             handle_lambda = @(t) (lambda_0 + gamma_a*(phi + (beta_s + gamma_d*sum_W)*t) + gamma_b*sum_W);
-            T_1 = 1/handle_lambda(tau); % let T_1 be 1 times of the current mean arrival time
+            T_1 = 200/handle_lambda(tau); % let T_1 be 1 times of the current mean arrival time
             delta_tau = NHPP(handle_lambda,T_1,tau);
             tau = tau + delta_tau;
         %% Step 3
@@ -69,10 +69,15 @@ R = zeros(samplesize,1);
 for i = 1:samplesize
     R(i) = 1 - (i+1)./(samplesize+3);
 end
+
+load('Rfunction.mat','R_1','t');
+plot(t,R_1,'r-');
+hold on;
+
 plot(t_f(:,1),R,'k-',t_f(:,2),R,'k--',t_f(:,3),R,'k:',t_f(:,4),R,'k-.');
 xlabel('t/s')
 ylabel('R(t)')
-% legend('\gamma_b=0','\gamma_b=10^-^5','\gamma_b=10^-^4','\gamma_b=10^-^3','Location','Southwest')
-legend('\gamma_c=0','\gamma_c=10^-^5','\gamma_c=10^-^4','\gamma_c=10^-^3','Location','Southwest')
+legend('\gamma_b=0','\gamma_b=0','\gamma_b=10^-^5','\gamma_b=10^-^4','\gamma_b=10^-^3','Location','Southwest')
+% legend('\gamma_c=0','\gamma_c=10^-^5','\gamma_c=10^-^4','\gamma_c=10^-^3','Location','Southwest')
 % legend('\gamma_d=0','\gamma_d=10^-^5','\gamma_d=10^-^4','\gamma_d=10^-^3','Location','Southwest')
 axis([0,6e4,0,1])
